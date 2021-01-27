c2076bb107d6997bcd8cdaba834568275c072a82
Francesco <francescoallera@gmail.com>
2021-01-27 09:40:56 +0100

    Exercise completed

---
+++ js/script.js
@@ -0,0 +1,13 @@
+new Vue({
+ el: '#root',
+ data: {
+   title: 'Inserisci qui i tuoi dati',
+   name: '',
+   surname: '',
+   image: 'http://via.placeholder.com/177',
+ }
+});
+
+
+
+Vue.config.devtools = true;
